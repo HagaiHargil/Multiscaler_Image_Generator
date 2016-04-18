705eeec44273face5fb927563f111531b32d6939
liorgolgher <liorgolgher@users.noreply.github.com>
2016-04-18 14:17:08 +0300

    Update DisplayOutcome.m
    3D extension - x,z,t

--- DisplayOutcome.m
+++ DisplayOutcome.m
@@ -1,10 +1,18 @@
+SumImage = sum(RawImage,3);
+ProdImage = prod(RawImage+1,3) - 1;
+ProdImage2 = prod(RawImage+0.5,3) - prod(0.5.*ones(size(RawImage)),3);
+
+RawImage2(:,:,1) = SumImage;
+RawImage2(:,:,2) = ProdImage ;
+RawImage2(:,:,3) = ProdImage2;
+
 figure;
 subplot(2,2,1)
-imshow(RawImage',[]); axis square;
+imshow(RawImage2,[]); axis square;colorbar;
 subplot(2,2,2)
-imshow(log(double(RawImage'+1)));colorbar; axis square;
-SmallImage = RawImage(:,8000:end); 
+imshow(log(double(RawImage2+1)));colorbar; axis square;
+SmallImage = RawImage2(:,round(size(RawImage2,2).*0.8):end,1); 
 subplot(2,2,3)
-imshow(SmallImage',[]);colorbar;axis square;
+imshow(SmallImage,[]);axis square;colorbar;
 subplot(2,2,4)
-imshow(log(double(SmallImage'+1)),[]);colorbar;axis square;
+imshow(log(double(SmallImage+1)),[]);;axis square;colorbar;
