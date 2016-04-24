8ebb7d23e99cae204baa9f2cfa4e9435c2110f08
Hagai Hargil <hagaihargil@gmail.com>
2016-04-24 17:50:35 +0300

    Made a few changes
    Cancelled flipping of even cells
    Added a loop to create an array CurrentFrameArray of the photons to be histogrammed - needs to be changed to NaNs.
    PhotonSpreadToImage2 is still not working, will address in next commit.

--- PhotonSpreadToImage2.m
+++ PhotonSpreadToImage2.m
@@ -1,16 +1,16 @@
-function [RawImage(:,:,m), C{m}]
+function [RawImage, C] = PhotonSpreadToImage2(PhotonCellArray, SizeX, SizeY, Num_of_Lines)
 %% Populating the raw image
 
-SizeX = 1500;
-SizeY = 150;
-RawImage = single(zeros(SizeX,SizeY,max(NumFrames,1)));
+
+RawImage = single(zeros(SizeX,SizeY));
 C = cell(NumFrames);
 % Building a histogram of photon locations
+[ RawImage(:,:,m), C{m} ] = hist3([],'Nbins',[SizeX SizeY], 'Edges', );
 
 for m = 1:NumFrames
     if numel(TotalHitsX{m})
         Minumel = min(numel(TotalHitsX{m}), numel(TotalHitsZ{m}));
-        [ RawImage(:,:,m), C{m} ] = hist3([TotalHitsX{m}(1:Minumel)  TotalHitsZ{m}(1:Minumel)],'Nbins',[SizeX SizeY]);
+        
 %         for n = 1:numel(TotalHitsX{m})
 % %             RawImage(round(LegalHitsX{m}(n)./ShrinkFactorX) +1 ,   round(LegalHitsZ{m}(n)./ShrinkFactorZ)+1, m) = 1 + RawImage(round(LegalHitsX{m}(n)./ShrinkFactorX) +1 ,   round(LegalHitsZ{m}(n)./ShrinkFactorZ)+1, m);
 %             RawImage(RescaledX{m}(n), RescaledZ{m}(n), m) = 1 + RawImage(RescaledX{m}(n), RescaledZ{m}(n), m);
